ed3ce11b763f2ea50140ae17dcb3797768c9f466
Adrian <adihg89@gmail.com>
2013-09-25 18:11:43 +0100

    CAmbio a master

--- popular.rb
+++ popular.rb
@@ -27,7 +27,7 @@ amigos.each do |a|
 end
 
 #lista_amigos.sort_by { |x,y| y }
-lista_amigos.sort! { |a,b| a_user2 = Twitter.user(a), a_user3 = Twitter.user(b), a_user2.follower_count <=> a_user3.follower_count }
+lista_amigos.sort{ |a,b| a_user2 = Twitter.user(a), a_user3 = Twitter.user(b), a_user2.follower_count <=> a_user3.follower_count }
 
 lista_amigos.each do |usu,num_seguidores|
 puts "usuario: #{usu} seguidores: #{num_seguidores}"
