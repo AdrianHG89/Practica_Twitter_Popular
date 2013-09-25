efc9eb6ff5272844967b017394960333761ba4cc
Adrian <adihg89@gmail.com>
2013-09-25 17:54:47 +0100

    Lista creada con usuario y numero de seguidores

--- popular.rb
+++ popular.rb
@@ -1,7 +1,7 @@
 require 'twitter'
 require './configure'
 
-screen_name = ARGV[0] || 'timoreilly'
+screen_name = ARGV[0] || 'Adrihg89'
 a_user = Twitter.user(screen_name)
 
 puts "Username   : #{a_user.screen_name}"
@@ -9,18 +9,33 @@ puts "Name       : #{a_user.name}"
 puts "Location   : #{a_user.location}"
 puts "URL        : #{a_user.url}" if a_user.url
 puts "Verified   : #{a_user.verified}"
+puts "Seguidores   : #{a_user.followers_count}"
 puts
 
 tweet = Twitter.user_timeline(screen_name).first
 
 amigos = Twitter.friends(screen_name)
-amigos.each { |a| a_user2 = Twitter.user(a)
-				print "Username   : #{a_user2.name} \n"}
-				
-
-
-if tweet
-  puts "Tweet text : #{tweet.text }"
-  puts "Tweet time : #{tweet.created_at}"
-  puts "Tweet ID   : #{tweet.id}"
+lista_amigos = {}
+#amigos.sort! { |a,b| a_user2 = Twitter.user(a), a_user3 = Twitter.user(b), a_user2.follower_count <=> a_user3.follower_count }
+amigos.each do |a|
+		#print "Username   : #{a.name} \n"
+		a_user2 = Twitter.user(a)
+		lista_amigos[a_user2.screen_name] = a_user2.followers_count
+		#print "Username: #{a_user2.name} Seguidores: #{a_user2.followers_count} \n"
+		#a_user2 = Twitter.user(a)
+        #print "Username   : #{a_user2.name} \n"
 end
+
+#lista_amigos.sort_by { |x,y| y }
+lista_amigos.sort! { |a,b| a_user2 = Twitter.user(a), a_user3 = Twitter.user(b), a_user2.follower_count <=> a_user3.follower_count }
+
+lista_amigos.each do |usu,num_seguidores|
+puts "usuario: #{usu} seguidores: #{num_seguidores}"
+end
+
+
+
+
+
+
+
